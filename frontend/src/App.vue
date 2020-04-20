707b0409a254d1325727d94b7b2235166ac5be81
kirandesimone <kirandesimone@yahoo.com>
2020-04-20 19:36:10 -0400

    login button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
-      <router-link v-if="authenticated" to="/login">Login</router-link>
+      <router-link v-if="getToken !== null" to="/login" v-on:click="logout()">Login</router-link>
     </div>
     <router-view/>
   </div>
