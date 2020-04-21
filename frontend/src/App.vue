37c24f7e79c87708848afbc5f3dd1f4e9ec126cb
Brooks <brooksgathagan@gmail.com>
2020-04-20 20:46:43 -0400

    Minor updates?

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,19 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
-      <div v-if= "token === null">
-        <router-link to="/login">Login</router-link>
-      </div>
-    
-    </div>
+      <router-link to="/login">Login</router-link> 
+    </div
     <router-view/>
   </div>
 </template>
 
 <script>
-// import auth from '../auth';
 
 export default {
+
   
 }
 </script>
