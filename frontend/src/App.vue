5357fcb8e36f29d40ed9055f915e03fa2b1edd8d
Alyssa Hernandez <alyssa@artivity.co>
2020-04-21 14:34:39 -0400

    maps works

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,14 +7,38 @@
       
     </div>
     <router-view/>
+    <div class="App"></div>
   </div>
 </template>
 
 <script>
+
+import gmapsInit from './utils/gmaps';
 import auth from '../src/auth';
 
 export default {
+  name: 'App',
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+
+      geocoder.geocode({ address: 'Austria' }, (results, status) => {
+        if (status !== 'OK' || !results[0]) {
+          throw new Error(status);
+        }
+
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
   components: {
+
   },
   computed: {
     loggedIn() {
@@ -28,6 +52,11 @@ export default {
 
 <style>
 #app {
+  width: 100vw;
+  height: 100vh;
+}
+
+.App {
   width: 100vw;
   height: 100vh;
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyCsnAc4fR8Sd-AqYRNkEhaH434Lm9r8trI',
-    libraries: 'directions', 
-  },
-})
 
 Vue.config.productionTip = false
 
