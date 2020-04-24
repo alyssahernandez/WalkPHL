6e5f221c59ea2ca4ef36bf05de2059404b8d491f
Brooks <brooksgathagan@gmail.com>
2020-04-23 21:25:11 -0400

    Committing to pull

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="yellow-background">
+        <p>Hi</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'side-bar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.yellow-background {
+    color: yellow
+}
+</style>
