b0b4baa799b59a38d98ce1b5b87e9d683fd285a1
Trevor Bowes <tpbowes0001@gmail.com>
2020-04-27 14:05:14 -0400

    work on destination create admin feature

--- frontend/src/views/AdminFeatures.vue
+++ frontend/src/views/AdminFeatures.vue
@@ -1,15 +1,105 @@
 <template>
     <div id="app" class="admin-features">
-      <h1> test </h1>
-
       <form>
-                <p><strong>Add a location?</strong></p>
-                <label for="location">
+                <p><strong>Add a destination</strong></p>
+                <label for="latitude">
+                  Latitude
                   <input 
                     type="text"
-                    v-model="addDestination.location"
-                    placeholder="Location"
-                    id="location"
+                    v-model="addDestination.latitude"
+                    placeholder="Latitude"
+                    id="latitude"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="longitude">
+                  Longitude
+                  <input 
+                    type="text"
+                    v-model="addDestination.longitude"
+                    placeholder="longitude"
+                    id="longitude"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="city">
+                  City
+                  <input 
+                    type="text"
+                    v-model="addDestination.city"
+                    placeholder="Philadelphia"
+                    id="city"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="state">
+                  State
+                  <input 
+                    type="text"
+                    v-model="addDestination.state"
+                    placeholder="PA"
+                    id="state"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="zipcode">
+                 Zipcode
+                  <input 
+                    type="text"
+                    v-model="addDestination.zipcode"
+                    placeholder="19103"
+                    id="zipcode"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="openFrom">
+                 OpenFrom time
+                  <input 
+                    type="text"
+                    v-model="addDestination.openFrom"
+                    placeholder="0000"
+                    id="openFrom"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="openTo">
+                 OpenTo time
+                  <input 
+                    type="text"
+                    v-model="addDestination.openTo"
+                    placeholder="0000"
+                    id="openTo"
+                    class="input"
+                    maxlength="30"
+                    required
+                    autofocus
+                  />
+                </label>
+                <label for="weekends">
+                 Open weekends? True/False
+                  <input 
+                    type="text"
+                    v-model="addDestination.weekends"
+                    placeholder="True"
+                    id="weekends"
                     class="input"
                     maxlength="100"
                     required
@@ -32,8 +122,14 @@ export default {
     data() {
       return {
           addDestination: {
-              location: '',
-              testOption: 'Option',
+              latitude: '',
+              longitude: '',
+              city: '',
+              state: '',
+              zipcode: '',
+              openFrom: '',
+              openTo: '',
+              weekends: ''
 
           },
     computed: {
