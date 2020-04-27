903cb68f53a91d61a6509aa103f94ea6bf10ff24
Trevor Bowes <tpbowes0001@gmail.com>
2020-04-27 11:07:29 -0400

    admin features work

---
+++ frontend/src/views/AdminFeatures.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app" class="admin-features">
+      <h1> test </h1>
+    </div>
+
+</template>
+
+<script>
+import auth from '../auth';
+export default {
+    name: 'admin-features',
+    components: {
+      
+    },
+    data() {
+      return {
+          menuOptions: {
+              addLocation: 'Add Location',
+              testOption: 'Option',
+
+          },
+    computed: {
+      userLoggedIn() {
+        return (auth.getToken() != null);
+      }
+      }}}};
+</script>
+
+<style>
+
+</style>
